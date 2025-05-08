73d130a6e9a7af44256e941f387b44e2eb07b22a
Elijah Lopez <elijahllopezz@gmail.com>
2025-05-08 21:57:10 +0000

    Source env after installing rustup

--- src/features/rust/install.sh
+++ src/features/rust/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+. "$HOME/.cargo/env"
 rustup component add rustfmt
 # https://dystroy.org/bacon/
 cargo install --locked bacon --features "clipboard sound"
