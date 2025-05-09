00aa05b1536575764d1bb8b73f338bc07a9e3bf6
Elijah Lopez <elijahllopezz@gmail.com>
2025-05-09 03:39:26 +0000

    log

--- src/features/frontend/install.sh
+++ src/features/frontend/install.sh
@@ -2,15 +2,16 @@
 set -e
 
 # nvm
+echo "installing nvm" > /etc/frontend-log
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash
 . "$HOME/.bashrc"
 nvm install --lts
 nvm use --lts
 corepack enable
-
+echo "installing bun" > /etc/frontend-log
 # bun
 curl -fsSL https://bun.sh/install | bash
-
+echo "installing typescript" > /etc/frontend-log
 # typescript
 npm install -g typescript
 npm install -g ts-node
